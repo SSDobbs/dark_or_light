dd8072e4ca755f90570dd882bb8817980c6b7490
Shyela Dobbs <shyela.webster@gmail.com>
2022-03-02 07:04:13 -0500

    Initial commit of the project with basic file structure and basic html pages.

---
+++ star-wars-scripts.js
@@ -0,0 +1,53 @@
+//character list//
+let starWarsPeopleList = document.querySelector('#characters');
+
+fetch('https://swapi.dev/api/people')
+.then(function(response) {   
+  return response.json(); 
+})
+.then(function(json) {
+  let people = json.results; 
+  let p = people;
+
+  for(p of people) {
+    let listItem = document.createElement('li'); 
+    listItem.innerHTML = '<p>' + p.name + '</p>'; 
+    starWarsPeopleList.appendChild(listItem);  
+  }
+});
+
+//planet list//
+let starWarsPlanetList = document.querySelector('#planets');
+
+fetch('https://swapi.dev/api/planets') 
+.then(function(response) {   
+  return response.json(); 
+})
+.then(function(json) {
+  let people = json.results; 
+  let p = people;
+
+  for(p of people) {
+    let listItem = document.createElement('li'); 
+    listItem.innerHTML = '<p>' + p.name + '</p>'; 
+    starWarsPlanetList.appendChild(listItem);  
+  }
+});
+
+//vehicle list//
+let starWarsVehicleList = document.querySelector('#vehicles');
+
+fetch('https://swapi.dev/api/vehicles/') 
+.then(function(response) {   
+  return response.json(); 
+})
+.then(function(json) {
+  let people = json.results; 
+  let p = people;
+
+  for(p of people) {
+    let listItem = document.createElement('li'); 
+    listItem.innerHTML = '<p>' + p.name + '</p>'; 
+    starWarsVehicleList.appendChild(listItem);  
+  }
+});
