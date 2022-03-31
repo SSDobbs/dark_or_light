a6102adcd70f0dad4b4882750a5d5447cdeddf14
Shyela Dobbs <shyela.webster@gmail.com>
2022-03-31 11:43:22 -0400

    Cleaned up code, added more to README, added media query, & made desktop version of site differ from mobile.

--- README.md
+++ README.md
@@ -1,10 +1,13 @@
-JavaScript Features:
-1. Pulling json data from SWAPI. 
+Hello
+This in my final project for CODE KY WEBDEV2. In this project I have a mobile layout in which the user can view the Star Wars movies (Episodes 1 through 6), list of characters, vehicles, and planets by clicking the links that take you to a separate page. On the desktop version all of the same information is shown on the index page. Additionally on the index page for both mobile & desktop a countdown timer for 'Star Wars' day. May the 4th be with you. If you are using firefox, you will see the animated lightsaber favicon. Other browsers do not support this animation aspect at this time.
+
+I used the following JavaScript Features:
+1. Pulling json data from SWAPI.
 2. Displaying the days until National Star Wars Day (May 4th).
-3. Movie.JSON file is parsed and display in the Movies List page with a decription & movie poster. The JSON file movie data was supplied by https://github.com/Package/Star-Wars-Express. 
-4. Star Wars Vehicle that move on the page. 
+3. Movie.JSON file is parsed and display in the Movies List page with a decription & movie poster. The JSON file movie data was supplied by https://github.com/Package/Star-Wars-Express.
+4. Star Wars Vehicle that move on the page in the mobile version.
 
-If you are using firefox, you will see the animated lightsaber favicon. Other browsers do not support this animation aspect at this time. 
+My project is hosted through Github pages and can be found at the following link: https://ssdobbs.github.io/dark_or_light/
+I hope you enjoy my project as much as I did while I was creating it and learning more javascript.
 
-
-https://ssdobbs.github.io/dark_or_light/
+Thank You
--- script/index.js
+++ script/index.js
@@ -1,13 +1,79 @@
 //days until National Star Wars Day May 4th
 let today = new Date();
-var swday= new Date(today.getFullYear(), 4, 3);
-if (today.getMonth()==4 && today.getDate()>3) 
-{
-swday.setFullYear(swday.getFullYear()+1); 
-}  
-var one_day=1000*60*60*24;
-document.getElementById("swday").innerHTML = (Math.ceil((swday.getTime()-today.getTime())/(one_day))+
-" days until National Star Wars Day!");
+var swday = new Date(today.getFullYear(), 4, 3);
+if (today.getMonth() == 4 && today.getDate() > 3) {
+    swday.setFullYear(swday.getFullYear() + 1);
+}
+var one_day = 1000 * 60 * 60 * 24;
+document.getElementById("swday").innerHTML = (Math.ceil((swday.getTime() - today.getTime()) / (one_day)) +
+    " days until National Star Wars Day!");
 
 
 
+fetch('../movies.json')
+    .then((response) => {
+        return response.json()
+    })
+    .then((res) => {
+        let moviesList = document.querySelector('#movie');
+        for (let i = 0; i < res.movies.length; i++) {
+            let movie = document.createElement('p');
+            movie.innerHTML = `<img src= ${res.movies [i].poster}><br>`;
+            movie.innerHTML += `<h3>${res.movies[ i ].title}</h3>`;
+            movie.innerHTML += `<button onclick= "window.open ('${res.movies[i].link}')></button><br>`;
+            let castList = '<ul>';
+            res.movies[i].main_characters.forEach((character) => {
+                castList += `<li>${character}</li>`;
+            })
+            castList += '</ul>'
+            movie.innerHTML += ` Cast: ${castList} <br>`;
+            movies.appendChild(movie);
+        }
+    });
+
+let starWarsPeopleList = document.querySelector('#characters');
+fetch('https://swapi.dev/api/people')
+    .then(function (res) {
+        return res.json();
+    })
+    .then(function (json) {
+        let people = json.results;
+        let p = people;
+        for (p of people) {
+            let listItem = document.createElement('p');
+            listItem.innerHTML = '<p>' + p.name + '</p>';
+            starWarsPeopleList.appendChild(listItem);
+        }
+    });
+
+let starWarsVehicleList = document.querySelector('#vehicles');
+fetch('https://swapi.dev/api/vehicles/')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        let people = json.results;
+        let p = people;
+
+        for (p of people) {
+            let listItem = document.createElement('p');
+            listItem.innerHTML = '<p>' + p.name + '</p>';
+            starWarsVehicleList.appendChild(listItem);
+        }
+    });
+
+let starWarsPlanetList = document.querySelector('#planets');
+fetch('https://swapi.dev/api/planets')
+    .then(function (response) {
+        return response.json();
+    })
+    .then(function (json) {
+        let people = json.results;
+        let p = people;
+
+        for (p of people) {
+            let listItem = document.createElement('p');
+            listItem.innerHTML = '<p>' + p.name + '</p>';
+            starWarsPlanetList.appendChild(listItem);
+        }
+    });
